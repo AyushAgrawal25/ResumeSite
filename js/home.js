2bfcb63271992cf9e7b6630dc4ae4f5d4a24529f
CoderAyush25 <agrawal.ayush25000@gmail.com>
2020-03-05 22:07:35 +0530

    About created

---
+++ js/home.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('qbApp', []);
+myApp.controller('qbCtrl', function($scope,$interval) {
+    var qbHeaderEle=angular.element(document.querySelector(".qb-header"));
+    angular.element(window).bind("scroll",function(){
+        if(window.scrollY>=150)
+        {
+            qbHeaderEle.addClass("qbColorGreen");
+        }
+        else
+        {
+            qbHeaderEle.removeClass("qbColorGreen");
+        }
+    });
+});
