bc43144d07fa1621ddf27c0b7af4e4948296efab
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2020-03-06 14:15:53 +0530

    Scroll Event Needs to be implemented

--- js/home.js
+++ js/home.js
@@ -1,6 +1,16 @@
 var myApp = angular.module('qbApp', []);
 myApp.controller('qbCtrl', function($scope,$interval) {
     var qbHeaderEle=angular.element(document.querySelector(".qb-header"));
+    if(window.scrollY>=150)
+    {
+        qbHeaderEle.addClass("qbColorGreen");
+    }
+    else
+    {
+        qbHeaderEle.removeClass("qbColorGreen");
+    }
+    var qbWowDivs=document.querySelectorAll(".wow2");
+    
     angular.element(window).bind("scroll",function(){
         if(window.scrollY>=150)
         {
@@ -10,8 +20,31 @@ myApp.controller('qbCtrl', function($scope,$interval) {
         {
             qbHeaderEle.removeClass("qbColorGreen");
         }
+
+        angular.forEach(qbWowDivs, function(qbWowDiv,key){
+            if((qbWowDiv.getBoundingClientRect().top>=50)&&(qbWowDiv.getBoundingClientRect().top<=(window.innerHeight-50)))
+            {
+                if(angular.element(qbWowDiv).attr("onDisplay")=="true")
+                {
+
+                }
+                else
+                {
+                    console.log("called");
+                    angular.element(qbWowDiv).attr("onDisplay","true");
+                    angular.element(qbWowDiv).addClass("fadeInUp");
+                }
+            }
+            else
+            {
+                if(angular.element(qbWowDiv).attr("onDisplay")=="true")
+                {
+                    angular.element(qbWowDiv).attr("onDisplay","false");
+                }
+                angular.element(qbWowDiv).removeClass("fadeInUp");
+            }
+        });
     });
 
-    var qbSlideInUpDivs=document.querySelectorAll(".qbSlideInUp");
-    console.log(qbSlideInUpDivs);
+    
 });
